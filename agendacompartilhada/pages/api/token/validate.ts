ef6887ebaa7277a06a41aa85074edfabfa85f2e3
GabrielOnohara <gabakio11@gmail.com>
2023-02-09 19:48:02 -0300

    Testando validacao de token

--- agendacompartilhada/pages/api/token/validate.ts
+++ agendacompartilhada/pages/api/token/validate.ts
@@ -1,18 +1,29 @@
-import { PrismaClient } from '@prisma/client';
-import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse } from 'next'
 const jwt = require('jsonwebtoken');
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const token = req.headers['x-access-token'];
-  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
   
-  jwt.verify(token, process.env.SECRET, function(err: any) {
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-
-    return res.status(200);
-  });
-  
+  switch (req.method) {
+    case 'POST':
+      try {
+        const token = req.headers['x-access-token'];
+        if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+      
+        jwt.verify(token, process.env.JWT_KEY, function(err: any) {
+          if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        });
+        return res.status(200).json({ auth: true, message: 'Token Válido' });
+     
+      } catch (error) {
+        return res.status(500).json({ auth: false, message: error });
+      }finally{
+        res.end()
+      }
+      default:
+        res.status(200).json({ name: 'John Doe' });
+        break;
+  }
 }
