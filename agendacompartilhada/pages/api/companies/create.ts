a2dd275b19537e95b4623850e3ce2f69e48377ea
GabrielOnohara <gabakio11@gmail.com>
2023-02-12 13:36:35 -0300

    Adicionando conexao prisma

--- agendacompartilhada/pages/api/companies/create.ts
+++ agendacompartilhada/pages/api/companies/create.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next'
+const jwt = require('jsonwebtoken');
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,8 +11,9 @@ export default async function handler(
     case "POST":
       const data =  req.body;
       const prisma = new PrismaClient();
+      await prisma.$connect()
       try {
-        const user = await prisma.company.findFirst({
+        const user = await prisma.company.findUnique({
           where: {
             email: data.email
           }
@@ -20,17 +22,19 @@ export default async function handler(
           res.statusMessage = "Usuário com o email já existe.";
           res.status(400);
         } else {
-          await prisma.company.create({
-            data: data
+          const newCompany  = await  prisma.company.create({
+            data : data
           })
+          const token = jwt.sign({newCompany}, process.env.JWT_KEY, {expiresIn: 60*60});
           res.statusMessage = "Usuário criado com sucesso.";
-          res.status(200);
+          res.status(200).json({company:newCompany, token});
         }
       } catch (error) {
         res.statusMessage = "Não foi possível criar usuário.";
         res.status(400).json({ error: error});
       }finally{
         res.end();
+        await prisma.$disconnect()
       }
       break;
     default:
