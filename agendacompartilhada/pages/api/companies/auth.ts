e205a7be73532e328c5f6928e40a34bf1301fc2a
GabrielOnohara <gabakio11@gmail.com>
2023-02-12 13:35:20 -0300

    Adicionando get company

--- agendacompartilhada/pages/api/companies/auth.ts
+++ agendacompartilhada/pages/api/companies/auth.ts
@@ -7,10 +7,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const data =  req.body;
+  const prisma = new PrismaClient();
+  await prisma.$connect()
   switch(req.method){
     case "POST":
-      const data =  req.body;
-      const prisma = new PrismaClient();
       try {
         const company = await prisma.company.findFirst({
           where: {
@@ -21,16 +22,8 @@ export default async function handler(
           var passwordsMatch = bcrypt.compareSync(data.password, company.password); 
           if(passwordsMatch){
             const token = jwt.sign({company}, process.env.JWT_KEY, {expiresIn: 60*60});
-            prisma.company.update({
-              where: {
-                email: data.email,
-              },
-              data: {
-                lastToken: token
-              }
-            })
             res.statusMessage = "Login efetuado com sucesso";
-            res.status(200).json({auth:true, token,company });
+            res.status(200).json({auth:true, token, company });
           }else{
             res.statusMessage = "Senha inválida";
             res.status(400);
@@ -44,10 +37,28 @@ export default async function handler(
         res.status(400).json({ error: error});
       }finally {
         res.end();
+        await prisma.$disconnect()
       }
       break;
     default:
-      res.status(200).json({ name: 'John Doe' });
+      try {
+        const company = await prisma.company.findUnique({
+          where: {
+            email: data.email,
+          }
+        });
+        if(company){
+          res.status(200).json({ company });
+        }else{
+          res.statusMessage = "Usuário não encontrado";
+          res.status(400);
+        }
+      }catch(error){
+        res.status(400).json({ error:error });
+      }finally{
+        res.end();
+        await prisma.$disconnect()
+      }
       break;
   }
 
