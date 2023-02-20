66d1e949e1d3a9617471b2be0f9e361d5f699597
GabrielOnohara <gabakio11@gmail.com>
2023-02-19 22:24:58 -0300

    Ajustando método de delete

--- agendacompartilhada/pages/api/contribuitors/delete/[id].ts
+++ agendacompartilhada/pages/api/contribuitors/delete/[id].ts
@@ -10,7 +10,7 @@ export default async function handler(
   const prisma = new PrismaClient();
   await prisma.$connect()
   switch(req.method){
-    case "POST":
+    case "DELETE":
       break;
     default:
       try{
