ce926d83e7ac38d7b77ee82b3f26380c049d2ddc
GabrielOnohara <gabakio11@gmail.com>
2023-02-19 23:09:24 -0300

    Retirando importacoes

--- agendacompartilhada/pages/_app.tsx
+++ agendacompartilhada/pages/_app.tsx
@@ -3,7 +3,6 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react';
-import UserStorage from '../src/context/UserContext';
 import TokenStorage from '../src/context/TokenContext';
 import CompanyStorage from '../src/context/CompanyContext';
 
