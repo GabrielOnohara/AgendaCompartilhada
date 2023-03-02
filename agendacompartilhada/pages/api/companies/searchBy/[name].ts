442299846061bbdf908438478815dd6ccdafbe11
GabrielOnohara <gabakio11@gmail.com>
2023-03-01 22:20:54 -0300

    Criando busca de empresas por nome

---
+++ agendacompartilhada/pages/api/companies/searchBy/[name].ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { nameSlug } = req.query;
+  const name =  nameSlug?.at(0); 
+  const prisma = new PrismaClient();
+  await prisma.$connect()
+  switch(req.method){
+    case "POST":
+      break;
+    default:
+      try{
+        const company = await prisma.company.findFirst({
+          where: { name: { endsWith: name} }
+        });
+        if(company){
+          res.status(200).json({company});
+        }else{
+          res.status(400).json({error: "Usuário não encontrado"});
+        }
+      }catch(error){
+        res.status(400).json({error: error});
+      }finally{
+        res.end();
+        await prisma.$disconnect();
+      }
+      break;
+  }
+}
