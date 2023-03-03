15c552ba45e9751ecd7100dd9a018675a69fa30a
GabrielOnohara <gabakio11@gmail.com>
2023-03-03 19:39:42 -0300

    Ajustando query de id

--- agendacompartilhada/pages/api/companies/searchById/[id].ts
+++ agendacompartilhada/pages/api/companies/searchById/[id].ts
@@ -5,8 +5,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { idSlug } = req.query;
-  const id =  idSlug?.at(0) ?? '0'; 
+  const query = req.query;
+  const id = query["id"] as string; 
   const prisma = new PrismaClient();
   await prisma.$connect()
   switch(req.method){
@@ -16,14 +16,14 @@ export default async function handler(
       try{
         const company = await prisma.company.findUnique({
           where: {
-            id: parseInt(id)
+            id: parseInt(id??'0')
           }
         });
         if(company){
           const {password, ...newCompany} = company;
           res.status(200).json({newCompany});
         }else{
-          res.status(400).json({error: "Empresa não encontrada"});
+          res.status(400).json({error: "Empresa não encontrada",});
         }
       }catch(error){
         res.status(400).json({error: error});
