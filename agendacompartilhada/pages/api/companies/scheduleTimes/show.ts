8218b02cc0a4069e3204c7072b2d502fac5839d9
GabrielOnohara <gabakio11@gmail.com>
2023-03-03 21:58:41 -0300

    ajustando ordenacao de data

--- agendacompartilhada/pages/api/companies/scheduleTimes/show.ts
+++ agendacompartilhada/pages/api/companies/scheduleTimes/show.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, ScheduleTime } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
@@ -27,11 +27,21 @@ export default async function handler(
             ]
           }
         });
+        
+        const scheduleTimesSortedByDate = scheduleTimes.sort( (a: ScheduleTime, b:ScheduleTime)=>{
+          if(new Date(a.date) < new Date(b.date)){
+            return 1;
+          }else{
+            return 0;
+          }
+        })
+
         if(scheduleTimes.length > 0){
-          res.status(200).json({scheduleTimes});
+          res.status(200).json({scheduleTimes: scheduleTimesSortedByDate});
         }else{
           res.status(200).json({scheduleTimes:[]});
         }
+        
       }catch(error){
         throw error
         res.status(400).json({error: error});
