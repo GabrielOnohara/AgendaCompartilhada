09b95a6c1459728145a6fdc7535cef6276da3aaf
GabrielOnohara <gabakio11@gmail.com>
2023-03-04 17:14:26 -0300

    Criando endpoint para agemdar horario

--- agendacompartilhada/pages/api/companies/scheduleTimes/create.ts
+++ agendacompartilhada/pages/api/companies/scheduleTimes/create.ts
@@ -6,40 +6,108 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const jsonData =  req.body;
-  const initialDate = jsonData.initialDate;
-  const endDate = jsonData.endDate;
+  // const data = {
+  //   client: {
+  //     email: clientEmail,
+  //     name: clientName,
+  //     phone: clientPhone,
+  //   },
+  //   companyId: company.id,
+  //   contributorId: contributorId, 
+  //   sheduleTime: {
+  //     date: dayjs(selectedScheduleDay).format('YYYY-MM-DD'),
+  //     time: selectedScheduleTime,
+  //     duaration: calendar.intervalTime,
+  //   },
+  // }
+  const clientData = jsonData.client;
   const companyId = jsonData.companyId;
+  const contribuitorId = jsonData.contributorId;
+  const scheduleTimeData = jsonData.scheduleTime;
   const prisma = new PrismaClient();
   await prisma.$connect()
   switch(req.method){
     case "POST":
-      try{
-        const scheduleTimes = await prisma.scheduleTime.findMany({
+      try{    
+        const company = await prisma.company.findUnique({
           where: {
-            AND:[
-              {companyId: parseInt(companyId)},
-              {
-                AND:[
-                  {date: {gt: new Date(initialDate)}},
-                  {date: {lt: new Date(endDate)}},
-                ]
-              }
-            ]
+            id: companyId
           }
         });
-        
-        const scheduleTimesSortedByDate = scheduleTimes.sort( (a: ScheduleTime, b:ScheduleTime)=>{
-          if(new Date(a.date) < new Date(b.date)){
-            return 1;
-          }else{
-            return 0;
-          }
-        })
 
-        if(scheduleTimes.length > 0){
-          res.status(200).json({scheduleTimes: scheduleTimesSortedByDate});
+        if(company){ 
+          const contribuitor = await prisma.contribuitor.findUnique({
+            where: {
+              id: contribuitorId,
+            }
+          })
+          if(contribuitor){
+            const client = await prisma.client.findUnique({
+              where: {
+                email: clientData.email,
+              }
+            });
+            if(client){
+              const scheduleTime = await prisma.scheduleTime.findFirst({
+                where: {
+                  companyId: companyId,
+                  date: new Date(scheduleTimeData.date),
+                  time: scheduleTimeData.time,
+                }
+              });
+
+              if(scheduleTime){
+                res.status(400).json({error: "Horário indisponível, atualize a página."});
+              }else{
+                const newScheduleTime = await prisma.scheduleTime.create({
+                  data: {
+                    companyId: company.id,
+                    date: new Date(scheduleTimeData.date),
+                    time: scheduleTimeData.time,
+                    duration: scheduleTimeData.duration,
+                    clientId: client.id,
+                    contribuitorId: contribuitor.id,
+                  }
+                });
+                if(newScheduleTime){
+                  res.status(200).json({newScheduleTime});
+                }else{
+                  res.status(400).json({error: "Não foi possível agendar horário"});
+                }
+              }
+            }else{
+              const newClient = await prisma.client.create({
+                data: {
+                  email:clientData.email,
+                  name: clientData.name,
+                  phone: clientData.phone,
+                }
+              });
+              if(newClient){
+                const newScheduleTime = await prisma.scheduleTime.create({
+                  data: {
+                    companyId: company.id,
+                    date: new Date(scheduleTimeData.date),
+                    time: scheduleTimeData.time,
+                    duration: scheduleTimeData.duration,
+                    clientId: newClient.id,
+                    contribuitorId: contribuitor.id,
+                  }
+                });
+                if(newScheduleTime){
+                  res.status(200).json({newScheduleTime});
+                }else{
+                  res.status(400).json({error: "Não foi possível agendar horário"});
+                }
+              }else{
+                res.status(400).json({error: "Não foi possível cadastrar cliente"})
+              }
+            }
+          }else{
+            res.status(400).json({error: "Profissional não encontrado"})
+          }
         }else{
-          res.status(200).json({scheduleTimes:[]});
+          res.status(400).json({error: "Empresa não encontrada"})
         }
         
       }catch(error){
