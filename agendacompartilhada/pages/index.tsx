79b193d6c8c1bfe0d6991751f17c0796a54511b1
GabrielOnohara <gabakio11@gmail.com>
2023-03-11 22:34:51 -0300

    Ajustar front do card de horario

--- agendacompartilhada/pages/index.tsx
+++ agendacompartilhada/pages/index.tsx
@@ -93,7 +93,7 @@ const Home: NextPage = () => {
         <Container>
           <Form onSubmit={handleSearchCompany}>
             <Form.Group className="mb-3">
-              <Form.Label className="darkBlueText mb-3"><h2>Pesquisar empresa</h2></Form.Label>
+              <Form.Label className="darkBlueText mb-4"><h2>Pesquisar empresa</h2></Form.Label>
               <Row  xs={12} md={12} sm={12}>
                 <Col xs={8} md={10} sm={9} >
                   <Form.Control type="text" placeholder="Digite o nome da empresa" value={searchValue} onChange={({ target }) => setSearchValue(target.value)}/>
@@ -111,73 +111,82 @@ const Home: NextPage = () => {
           {companies.map((company,index) => (
             <Col key={index}> 
               <Card style={{border: "1px solid #034078"}}>
-              <Card.Body>
-                <Row xs={12} md={12}>
-                  <Col xs={12} sm={4} md={2}>
-                    <Card.Img variant="top" src="/avatarimage.jpg" style={{width: "130px", borderRadius: "80%"}} className="mx-auto"/>
-                  </Col>
-                  <Col  xs={12} sm={8} md={10}>
-                    <Card.Title className="darkBlueText  mt-2 mb-3">{company.name}</Card.Title>
-                    <Card.Text style={{float: "left",}}>
-                      <p className="mb-2"><span className={`darkBlueText`}>Endereço:</span> Rua das amélias 999, Guarulhos-SP {company.address}</p>
-                      <p className=""><span className={`darkBlueText`}>Telefone:</span> {company.phone}</p>
-                    </Card.Text>
-                    <Button style={{float:"right",}} variant="primary" className="ms-auto mt-3" onClick={() => redirectToCompanyPage(company.id)}>Acessar</Button>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card> 
+                <Card.Body>
+                  <Row xs={12} md={12}>
+                    <Col xs={12} sm={4} md={2}>
+                      <Card.Img variant="top" src="/avatarimage.jpg" style={{width: "130px", borderRadius: "80%"}} className="mx-auto"/>
+                    </Col>
+                    <Col  xs={12} sm={8} md={10}>
+                      <Card.Title className="darkBlueText  mt-2 mb-3">{company.name}</Card.Title>
+                      <Card.Text style={{float: "left",}}>
+                        <p className="mb-2"><span className={`darkBlueText`}>Endereço:</span> Rua das amélias 999, Guarulhos-SP {company.address}</p>
+                        <p className=""><span className={`darkBlueText`}>Telefone:</span> {company.phone}</p>
+                      </Card.Text>
+                      <Button style={{float:"right",}} variant="primary" className="ms-auto mt-3" onClick={() => redirectToCompanyPage(company.id)}>Acessar</Button>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card> 
             </Col>
           ))}
           </Row>
           <hr />
-          <h2 className="darkBlueText my-3">Consulte seus horários</h2>
-          <Form >
-            <Form.Group className="mb-3">
-              <Form.Label className="darkBlueText"><b>Digite seu email</b></Form.Label>
-              <Row  xs={12} md={12} sm={12}>
-                <Col xs={12} md={6} sm={12} >
-                  <Form.Control type="text" placeholder="Digite seu email" value={searchValue} onChange={({ target }) => setSearchValue(target.value)}/>
-                </Col>
-                <Col xs={12} md={12} sm={12} >
+          <Row>
+            <Col md={6}>
+              <h2 className="darkBlueText my-4">Consulte seus horários</h2>
+              <Form >
+                <Form.Group className="mb-3">
+                  <Form.Label className="darkBlueText"><b>Digite seu email</b></Form.Label>
+                  <Row  xs={12} md={12} sm={12}>
+                    <Col xs={12} md={9} sm={12} >
+                      <Form.Control type="text" placeholder="Digite seu email" value={searchValue} onChange={({ target }) => setSearchValue(target.value)}/>
+                    </Col>
+                    <Col xs={12} md={12} sm={12} >
 
-                </Col>
-              </Row>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label className="darkBlueText" style={{display:"block"}}><b>Selecione a data</b> </Form.Label>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  value={date}
-                  onChange={(newValue) => {    
-                    setDate(dayjs(newValue));
-                  }}
-                  renderInput={(params) => <TextField {...params} />}
-                  inputFormat="DD/MM/YYYY"
-                  className={styles.datePicker}
-                />
-              </LocalizationProvider>
-            </Form.Group>
-            <Button variant="success"  >Confirmar</Button>
-          </Form>
+                    </Col>
+                  </Row>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label className="darkBlueText" style={{display:"block"}}><b>Selecione a data</b> </Form.Label>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      value={date}
+                      onChange={(newValue) => {    
+                        setDate(dayjs(newValue));
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                      inputFormat="DD/MM/YYYY"
+                      className={styles.datePicker}
+                    />
+                  </LocalizationProvider>
+                </Form.Group>
+                <Button variant="success mt-2"  >Confirmar</Button>
+              </Form>
+            </Col>
+            <Col md={6}>
+              <Card className="my-4">
+                <Card.Body>
+                  <Card>
+                    <Card.Body>
+                      <Row xs={12} md={12}>
+                        <Col  xs={12} sm={8} md={10}>
+                          <Card.Title className="darkBlueText  mt-2 mb-3">Horário 1</Card.Title>
+                          <Card.Text style={{float: "left",}}>
+                            <p className="mb-2"><span className={`darkBlueText`}>Data:</span> 11/03/2023</p>
+                            <p className="mb-2"><span className={`darkBlueText`}>Horário:</span> 8:00 </p>
+                            <p><span className={`darkBlueText`}>Endereço</span> Rua das amélias 999, Guarulhos-SP </p>
+                          </Card.Text>
+                        </Col>
+                      </Row>
+                    </Card.Body>
+                  </Card> 
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
           {/* <h2 className="darkBlueText mt-4 mb-3">Empresas recentes</h2>
           <Card style={{border: "1px solid #034078"}}>
-            <Card.Body>
-              <Row xs={12} md={12}>
-                <Col xs={12} sm={3} md={2}>
-                <Card.Img variant="top" src="/avatarimage.jpg" style={{width: "130px", borderRadius: "80%"}} className="mx-auto"/>
-                </Col>
-                <Col  xs={12} sm={9} md={10}>
-                  <Card.Title className="darkBlueText  mt-2 mb-3">Special title treatment</Card.Title>
-                  <Card.Text style={{float: "left",}}>
-                    <p className="mb-2"><span className={`darkBlueText`}>Endereço:</span> Rua das amélias 999, Guarulhos-SP</p>
-                    <p className=""><span className={`darkBlueText`}>Telefone:</span> 1199999999</p>
-                    
-                  </Card.Text>
-                  <Button style={{float:"right",}} variant="primary" className="ms-auto mt-3">Acessar</Button>
-                </Col>
-              </Row>
-            </Card.Body>
+            
           </Card> */}
         </Container> 
       </main>
