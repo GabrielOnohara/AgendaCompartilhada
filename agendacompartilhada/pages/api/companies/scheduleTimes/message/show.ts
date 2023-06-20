ab17baec375c847a9f89272cee7c5df3a35d2f9c
GabrielOnohara <gabakio11@gmail.com>
2023-06-20 16:37:52 -0300

    Ajustando codigo no enpoint de leitura

--- agendacompartilhada/pages/api/companies/scheduleTimes/message/show.ts
+++ agendacompartilhada/pages/api/companies/scheduleTimes/message/show.ts
@@ -48,22 +48,11 @@ export default async function handler(
           res.statusMessage = "Não encontrou empresa";
           res.status(400).json({ error: "Não encontrou empresa" });
         }
-        const message = await prisma.message.create({
-          data: {
-            content: jsonData.message,
-            scheduleTimeId: jsonData.scheduleTimeId,
-          },
-        });
-        if (message) {
-          res.statusMessage = "Mensagem enviada com sucesso";
-          res.status(200).json({ message });
-        } else {
-          res.statusMessage = "Erro ao mandar mensagem";
-          res.status(400).json({ error: "Erro ao mandar mensagem" });
-        }
+
       } catch (error) {
-        res.statusMessage = "Erro ao mandar mensagem";
+        res.statusMessage = "Erro: " + error;
         res.status(400).json({ error: error });
+        throw error
       } finally {
         res.end();
         await prisma.$disconnect();
