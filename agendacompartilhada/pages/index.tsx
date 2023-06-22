05d09641963a0aa91634ea59b6b25f0b1872b63a
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2023-06-22 18:49:43 -0300

    FIX Espaço no texto de endereço
    Agora tem o espaço separando o texto Endereço: do endereço armazenado ao buscar a empresa

--- agendacompartilhada/pages/index.tsx
+++ agendacompartilhada/pages/index.tsx
@@ -387,7 +387,7 @@ const Home: NextPage = () => {
                         </Card.Title>
                         <Card.Text style={{ float: "left" }}>
                           <p className="mb-2">
-                            <span className={`darkBlueText`}>Endereço:</span>
+                            <span className={`darkBlueText`}>Endereço:</span>{" "}
                             {company.address}
                           </p>
                           <p className="">
